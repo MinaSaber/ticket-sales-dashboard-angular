0fff165d519cefb1c3364c5d5ad5f0543d5ec1a0
MinaSaber <mina.saper.12@gmail.com>
2024-09-05 19:33:35 +0300

    table not completed

---
+++ src/app/dtos/user-dto.ts
@@ -0,0 +1,5 @@
+export interface User {
+    name: string;
+    mobileNumber: string;
+    photo: string;
+}
--- src/app/table-view/table-view.component.ts
+++ src/app/table-view/table-view.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { TableService } from '../services/table-mock';
+import { Table } from '../dtos/table-view-dto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table-view',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CommonModule],
   templateUrl: './table-view.component.html',
-  styleUrl: './table-view.component.scss'
+  styleUrl: './table-view.component.scss',
 })
-export class TableViewComponent {
+export class TableViewComponent implements OnInit {
+  constructor(private tableService: TableService) {}
 
+  tickets: Table[] = [];
+
+  ngOnInit(): void {
+    this.tickets = this.tableService.tickts;
+  }
 }
